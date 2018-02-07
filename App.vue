976503ae51301c363d7d4a93e6b001ce8203a77d
Darel Johnson <dareljohnson@yahoo.com>
2018-02-07 15:47:21 -0500

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <v-app id="inspire" dark>
+            <Navbar></Navbar>
+            <v-content>
+                <v-container fluid fill-height>
+                    <v-layout justify-center align-center>
+                        <router-view></router-view>
+                    </v-layout>
+                </v-container>
+            </v-content>
+            <v-footer app fixed>
+                <span>&copy; 2017</span>
+            </v-footer>
+        </v-app>
+    </div>
+</template>
+<script>
+import Navbar from "../components/layout/Navbar";
+
+export default {
+    name: "app",
+    components: {
+        Navbar
+    }
+};
+</script>
