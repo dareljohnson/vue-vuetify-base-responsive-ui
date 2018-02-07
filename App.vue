4b4c4970875c561f348fed96ef351dd672cffbaa
Darel Johnson <dareljohnson@yahoo.com>
2018-02-07 16:23:00 -0500

    Updates to baseline file structure

--- App.vue
+++ App.vue
@@ -1,26 +1,23 @@
 <template>
-    <div id="app">
-        <v-app dark>
-            <v-navigation-drawer app></v-navigation-drawer>
-            <v-toolbar app fixed></v-toolbar>
-            <v-content>
-                <v-container fluid fill-height>
-                    <v-layout>
-                        <router-view></router-view>
-                    </v-layout>
-                </v-container>
-            </v-content>
-            <v-footer app fixed>
-                <span>&copy; 2017</span>
-            </v-footer>
-        </v-app>
-    </div>
+  <v-app dark>
+      <v-navigation-drawer app></v-navigation-drawer>
+      <v-toolbar app fixed>App</v-toolbar>
+      <v-content>
+          <v-container fluid fill-height>
+              <v-layout>
+                  <router-view></router-view>
+              </v-layout>
+          </v-container>
+      </v-content>
+      <v-footer app fixed>
+          <span>&copy; 2017</span>
+      </v-footer>
+  </v-app>
 </template>
 <script>
 // imports go here
 
 export default {
-    name: "app",
     components: {
     
     }
